0e0d85258639262665f90fcc8e069a34732b2838
Swarnim Burnwal <98446769+SWARNIM-BURNWAL@users.noreply.github.com>
2025-03-31 17:45:31 +0530

    fixed deployment issue

--- components/component/Perks.tsx
+++ components/component/Perks.tsx
@@ -1,39 +1,46 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Lottie from "lottie-react";
+
 import Wifi from "../../lib/wired-lineal-64-wifi.json";
 import Kimono from "../../lib/wired-lineal-1788-kimono.json";
-
 import Fries from "../../lib/wired-outline-567-french-fries-chips.json";
 import Animation1 from "../../lib/1.json";
 
-
 export default function Perks() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const styling = {
     height: 120,
     width: 120,
   };
 
   return (
-    <div className="flex flex-wrap text-center  gap-10 justify-center items-center font-bold select-none ">
-      <div>
-        <Lottie animationData={Wifi} style={styling} />
-        <p> Good Connectivity</p>
-      </div>
-      <div>
-        <Lottie animationData={Fries} style={styling} />
-        <p> Free Food</p>
-      </div>
-     
-      <div>
-        <Lottie animationData={Kimono} style={styling} />
-        <p> Swags & Merch</p>
-      </div>
-      <div>
-        <Lottie animationData={Animation1} style={styling} />
-        <p> Mentorships</p>
-      </div>
-     
+    <div className="flex flex-wrap text-center gap-10 justify-center items-center font-bold select-none">
+      {isClient && (
+        <>
+          <div>
+            <Lottie animationData={Wifi} style={styling} />
+            <p>Good Connectivity</p>
+          </div>
+          <div>
+            <Lottie animationData={Fries} style={styling} />
+            <p>Free Food</p>
+          </div>
+          <div>
+            <Lottie animationData={Kimono} style={styling} />
+            <p>Swags & Merch</p>
+          </div>
+          <div>
+            <Lottie animationData={Animation1} style={styling} />
+            <p>Mentorships</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
